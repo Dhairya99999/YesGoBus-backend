65801832c94176c3fdf479acb17d2d972f6a4a1c
Balaji798 <balajiy798@gmail.com>
2024-04-12 10:52:17 +0530

    new update

--- route/user.js
+++ route/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const subAdminController = require("../controllers/user");
 const middleware = require("../middleware/authenticateUser");
 router.post("/signup", subAdminController.user_signup);
+router.post("/login",subAdminController.user_login)
 // paAEjrFNY0dwvwOg balajiy798
 
 module.exports = router;

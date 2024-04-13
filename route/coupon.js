0266dad0c65a016d787f85a3ab323489c7796dfd
Balaji798 <balajiy798@gmail.com>
2024-04-13 16:56:02 +0530

    new update

--- route/coupon.js
+++ route/coupon.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const couponController = require("../controllers/coupon");
+const middleware = require("../middleware/authenticateUser");
 
 router.post("/add_coupon",couponController.add_coupon);
-router.post("/apply_coupon_code",couponController.apply_coupon_discount)
+router.post("/apply_coupon_code",middleware.authenticateToken,couponController.apply_coupon_discount)
 
 module.exports = router;

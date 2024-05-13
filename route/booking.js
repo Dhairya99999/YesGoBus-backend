5c6eb7049e268a25e590db97ac01204744f15a80
Balaji Yadav <93571474+Balaji798@users.noreply.github.com>
2024-05-13 13:13:07 +0530

    Update booking.js

--- route/booking.js
+++ route/booking.js
@@ -8,10 +8,11 @@ const storiesModel = require("../model/travelStories")
 router.post("/book_hotel",middleware.authenticateToken,bookingController.make_booking);
 router.post("/add_itinerary_plans",bookingController.add_itinerary_plans)
 router.post("/itinerary_plans",bookingController.get_Itinerary_plans )
-router.post("/update_booking",middleware.authenticateToken,bookingController.edit_booking)
+router.post("/update_booking",middleware.authenticateToken,bookingController.edit_booking);
+router.post("/update_booking_payment",middleware.authenticateToken,bookingController.update_booking_payment)
 router.post("/add_booking_query",middleware.authenticateToken,bookingController.customer_sport)
 router.get("/get_user_booking", middleware.authenticateToken, bookingController.get_customer_booking)
-router.get("/get_booking",  middleware.authenticateToken, bookingController.get_booking)
+router.post("/get_booking",  middleware.authenticateToken, bookingController.get_booking)
 
 router.post("/add_stories",async(req,res)=>{
     try{

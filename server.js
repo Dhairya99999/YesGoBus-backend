ef5c085194172d748d64a4bdc9138fb9c9b2d10b
Balaji798 <balajiy798@gmail.com>
2024-08-08 16:43:47 +0530

    new update

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.json({ limit: '100mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://yesgobus.com','https://yegobus-web.onrender.com', 'https://yesgobus.netlify.app', 'https://localhost', 'http://192.168.1.101:5173', 'http://localhost'],
+    origin: ['http://localhost:5173', 'https://yesgobus.com', 'https://yesgobus.netlify.app', 'https://localhost', 'http://192.168.1.101:5173', 'http://localhost'],
     credentials: true,
   })
 );
